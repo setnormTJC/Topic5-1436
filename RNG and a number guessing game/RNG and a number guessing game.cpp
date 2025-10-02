ee56627d4e2f678a52056ea39ad9039c4181447a
Work <Work@MSI>
2025-10-02 10:06:27 -0500

    Where we ended on Oct. 2.
    Resume next time with for loops.

--- RNG and a number guessing game/RNG and a number guessing game.cpp
+++ RNG and a number guessing game/RNG and a number guessing game.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include<random> 
 
+
 int generateRandomNumberBetween0AndN(const int N)
 {
 	std::mt19937 rng(std::random_device{}());
@@ -13,12 +14,11 @@ int generateRandomNumberBetween0AndN(const int N)
 	return randomDistribution(rng); 
 }
 
-
-int main()
+void demoSimpleRNG()
 {
 	//the "deprecated" method of RNG: 
 
-	int counter = 0; 
+	int counter = 0;
 	srand(time(0)); //time(0) -> what do it do???
 	//s stands for "seed" (whatever that means), let's just abstract away the detail and use the result
 
@@ -33,14 +33,58 @@ int main()
 		int randomNumberBetween0AndN = generateRandomNumberBetween0AndN(N);
 		std::cout << randomNumberBetween0AndN << "\n";
 
-		counter++; 
+		counter++;
+	}
+}
+
+
+int main()
+{
+	const int N = 100; 
+	int correctNumber = generateRandomNumberBetween0AndN(N);
+
+	int numberOfGuesses = 0; 
+	std::cout << "Dirty cheater! The number is " << correctNumber << "\n";
+
+	bool gameWon = false; 
+	const int MAX_GUESSES_ALLOWED = 6; 
+
+	//for() //next time
+
+	while (!gameWon && numberOfGuesses < MAX_GUESSES_ALLOWED)
+	{
+		std::cout << "GUESS the number:\n";
+		int usersGuess{}; //braces initiliaze an int to 0
+		std::cin >> usersGuess;
+
+		numberOfGuesses++; 
+
+		if (usersGuess == correctNumber)
+		{
+			gameWon = true; 
+			//std::cout << "You won!\n";
+		}
+
+		else if (usersGuess > correctNumber)
+		{
+			std::cout << "That was too HIGH!\n";
+		}
+
+		else //THIS means usersGuess is LESS THAN the correct number
+		{
+			std::cout << "Too LOW!\n";
+		}
 	}
 
-	//make a rn guessing game when we come back in 15 minutes 
+	if (!gameWon)
+	{
+		std::cout << "Too many guesses - lost\n";
+	}
 
+	else
+	{
+		std::cout << "Huzzah!\n";
+	}
 
-	//ONE limitation of rand() -> RAND_MAX
-
-	//a preferable alternative (masked with abstraction): 
 
 }
